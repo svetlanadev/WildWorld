0259685969a6719127d14c93ea6352ad93c4ddc4
Svetadev <turistenok1@gmail.com>
2016-02-25 17:47:10 +0200

    Next commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_lenth=128, unique=True)
+
+	def __str__(self):
+		return self.name
+
+#	def __unicode__(self):
+#		return self.name
--- wildworld/urls.py
+++ wildworld/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
-#	url(r'^$', TemplateView.as_view(template_name='index.html'), name = 'home'),
+	url(r'^$', TemplateView.as_view(template_name='index.html'), name = 'home'),
 #	url(r'^about/', AboutView.as_view()),
 ]
 
