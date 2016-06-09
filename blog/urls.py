623065a1ee4e643f2a72b462e5b2b74189526743
Svetadev <turistenok1@gmail.com>
2016-06-09 20:56:28 +0300

    new design of Blog

--- blog/urls.py
+++ blog/urls.py
@@ -5,11 +5,14 @@ from blog import views
 
 urlpatterns = [
 	url(r'^$', views.index),
+	url(r'^index/', views.index, name='index'),
 	url(r'^o_nas', views.o_nas),
 	url(r'^sveta_contact', views.sveta_contact),
-	url(r'^post/([0-9]+)$', views.post, name = 'post_num'),
+	url(r'^allposts/(.*)', views.allposts, name='allposts'),
+	url(r'^allposts/post/([0-9]+)$', views.post, name = 'post_num'),
+	url(r'^allcategory/(.*)', views.allcategory, name='allcategory'),
 	url(r'^category/(.*)', views.categoryDisplay),
-	url(r'^poems/([0-9]+)$', views.poems),
+	url(r'^poems', views.poems, name='poems'),
 	url(r'^(?P<question_id>\d+)/results/$', views.results, name="results"),
     url(r'^(?P<question_id>\d+)/vote/$', views.vote, name="vote"),
 ]
@@ -21,3 +24,4 @@ urlpatterns = [
 
 #urlpatterns = patterns('',
 #	url(r'^$', 'views.index', name = 'index'))
+ 	# url(r'^allcategory/category/(.*)', views.categoryDisplay),
--- blog/views.py
+++ blog/views.py
@@ -7,15 +7,36 @@ from blog.models import Post, Category, Poems
 class MyStruct(object):
 		pass
 
-def home_page(request):
-    return HttpResponse('<html><title>Blog lists</title>Hello Blog <br> "You must be joking!" I can hear you say.</html>')
-
 
 def index(request):
 	posts = Post.objects.all()
-	categories_any_variable = Category.objects.all()
+	categ = Category.objects.all()
 	poems_variable = Poems.objects.all()
-	return render(request, 'index.html', {'lamps' : posts, 'category_sequence' : categories_any_variable, 'allpoems' : poems_variable})
+	return render(request, 'index.html', {'lamps' : posts, 'category_sequence' : categ, 'allpoems' : poems_variable})
+
+def allposts(request, allposts_num):
+	posts = Post.objects.all()
+	return render(request, 'allposts.html', {'lamps' : posts})
+
+
+def post(request, post_num):
+	post = Post.objects.get(id=post_num)
+	# post = {'title' : 'myTitle', 'body' : 'my body'}
+	return render(request, 'post.html', {'p' : post})
+
+def allcategory(request, allcateg_num):
+	categ = Category.objects.all()
+	return render(request, 'all_categories.html', {'category_sequence' : categ})
+
+def categoryDisplay(request, categ_name):
+	categoryItem = Category.objects.get(id=categ_name)
+	return render(request, 'category_one.html', {'categoryItem' : categoryItem, 'lamps': Post.objects.filter(category=categoryItem)})
+
+
+def poems(request, poem_num):
+	poemses = Poems.objects.get(id=poem_num)
+	return render(request, 'poems.html', {'allpoems' : poemses})
+
 
 def o_nas(request):
 	return render(request, 'o_nas.html')
@@ -24,21 +45,10 @@ def sveta_contact(request):
 	return render(request, 'sveta_contact.html',)
 
 
-def post(request, post_num):
-	post = Post.objects.get(id=post_num)
-	# post = {'title' : 'myTitle', 'body' : 'my body'}
-	return render(request, 'post.html', {'banana' : post})
 
 
-def categoryDisplay(request, categ_name):
-	categoryItem = Category.objects.get(id=categ_name)
-	return render(request, 'category_one.html', {'categoryItem' : categoryItem, 'lamps': Post.objects.filter(category=categoryItem)})
 
-
-def poems(request, poem_num):
-	poem = Poems.objects.get(id=poem_num)
-	return render(request, 'poems.html', {'allpoems' : poem})
-
+# ----  few functioons just for training 
 
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
