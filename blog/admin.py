80b938ec1365ff41f58284c155d51456aeef7852
Svetik <turistenok1@gmail.com>
2016-07-30 09:22:46 +0300

    some changes (cherry picked from commit e7ce01e)

--- blog/admin.py
+++ blog/admin.py
@@ -39,6 +39,6 @@ class PostAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Category)
+admin.site.register(UserProfile)
 admin.site.register(Post, PostAdmin)
 # admin.site.register(Poems)
-admin.site.register(UserProfile)
